22c096704f122cafd85e995c9486f5494e77cb6f
CapitanFindusFI <am95.dev@gmail.com>
2020-01-29 15:48:48 +0100

    1.0.12

--- src/client/graphql-generator/index.ts
+++ src/client/graphql-generator/index.ts
@@ -1,8 +1,8 @@
 /* tslint:disable:variable-name */
-import { isEmpty, isObject, set } from "lodash";
-import { GraphQLOperationType } from "../../enums";
-import { IGraphQLParam } from "../../interfaces";
-import { GraphQLRequest } from "../graphql-request";
+import { isEmpty, isObject, set } from 'lodash';
+import { GraphQLOperationType } from '../../enums';
+import { IGraphQLParam } from '../../interfaces';
+import { GraphQLRequest } from '../graphql-request';
 
 export class GraphQLGenerator {
   get parameters(): { [p: string]: string } {
@@ -16,9 +16,11 @@ export class GraphQLGenerator {
   public static generateWrapper(operationType: string, params: { [p: string]: string }): string {
     let queryString = operationType;
     if (Object.keys(params).length) {
-      const headerParams: string = Object.keys(params).map((key: string) => {
-        return `$${key}:${params[key]}`;
-      }).join(",");
+      const headerParams: string = Object.keys(params)
+        .map((key: string) => {
+          return `$${key}:${params[key]}`;
+        })
+        .join(',');
       queryString += `(${headerParams})`;
     }
 
@@ -26,15 +28,15 @@ export class GraphQLGenerator {
   }
 
   public static generateFragment(name: string, params: IGraphQLParam[], fields: string[]): string {
-    return [GraphQLGenerator.generateHeader(name, params), GraphQLGenerator.generateFields(fields)].join("");
+    return [GraphQLGenerator.generateHeader(name, params), GraphQLGenerator.generateFields(fields)].join('');
   }
 
   public static generateHeader(name: string, params: IGraphQLParam[]): string {
-    let headerParams = "";
+    let headerParams = '';
     if (params.length) {
       const queryString = params.map((param: IGraphQLParam) => {
         const fieldName = GraphQLGenerator.getParamQueryName(param);
-        return [fieldName, `$${fieldName}`].join(":");
+        return [fieldName, `$${fieldName}`].join(':');
       });
 
       headerParams = `(${queryString})`;
@@ -54,7 +56,7 @@ export class GraphQLGenerator {
       Object.keys(item).forEach((key: string) => {
         const itemValue: any = item[key];
         if (isObject(itemValue)) {
-          unwrappedQuery = unwrappedQuery.concat(key, "{", unwrapItem(itemValue), "}");
+          unwrappedQuery = unwrappedQuery.concat(key, '{', unwrapItem(itemValue), '}');
         } else {
           unwrappedQuery.push(key);
         }
@@ -62,18 +64,18 @@ export class GraphQLGenerator {
       return unwrappedQuery;
     };
 
-    const queryStrings = ["{", ...unwrapItem(queryObject), "}"];
-    return queryStrings.join(" ");
+    const queryStrings = ['{', ...unwrapItem(queryObject), '}'];
+    return queryStrings.join(' ');
   }
 
   private static getParamQueryName(param: IGraphQLParam): string {
     if (!param.name) {
-      throw new Error("GraphQL param is missing its name");
+      throw new Error('GraphQL param is missing its name');
     }
 
     let paramName;
     if (param.alias) {
-      const paramString = param.alias.split("$");
+      const paramString = param.alias.split('$');
       paramName = paramString[paramString.length - 1];
     } else {
       paramName = `${param.name}`;
@@ -93,10 +95,10 @@ export class GraphQLGenerator {
 
   constructor(operationType: GraphQLOperationType, ...requests: GraphQLRequest[]) {
     if (!operationType) {
-      throw new Error("No operation type provided");
+      throw new Error('No operation type provided');
     }
     if (!requests.length) {
-      throw new Error("No requests provided");
+      throw new Error('No requests provided');
     }
 
     this.operationType = operationType;
@@ -110,7 +112,7 @@ export class GraphQLGenerator {
 
   public generateQueryString(): string {
     const queryWrapper = GraphQLGenerator.generateWrapper(this.operationType, this._parameters);
-    const queryFragments = this.requests.map(GraphQLGenerator.generateRequestQuery).join(" ");
+    const queryFragments = this.requests.map(GraphQLGenerator.generateRequestQuery).join(' ');
 
     return `${queryWrapper}{${queryFragments}}`;
   }
@@ -128,7 +130,7 @@ export class GraphQLGenerator {
         .map(p => {
           return {
             name: GraphQLGenerator.getParamQueryName(p),
-            type: p.type
+            type: p.type,
           };
         })
         .forEach(item => {
@@ -136,7 +138,7 @@ export class GraphQLGenerator {
             const itemType: string | undefined = obj[item.name];
             if (itemType !== item.type) {
               throw new Error(
-                `Param ${item.name} can't be duplicated with a different type. current type: ${itemType}, trying to set: ${item.type}`
+                `Param ${item.name} can't be duplicated with a different type. current type: ${itemType}, trying to set: ${item.type}`,
               );
             }
           } else {
--- src/client/graphql-request/query.ts
+++ src/client/graphql-request/query.ts
@@ -1,12 +1,12 @@
-import { IGraphQLParam } from "../../interfaces";
-import { GraphQLRequest } from "./index";
+import { IGraphQLParam } from '../../interfaces';
+import { GraphQLRequest } from './index';
 
 class GraphQLQueryRequest extends GraphQLRequest {
   constructor(
     requestName: string,
     resultFields: string[],
     requestParams: IGraphQLParam[] = [],
-    requestValues: { [p: string]: any } = {}
+    requestValues: { [p: string]: any } = {},
   ) {
     super(requestName, resultFields, requestParams, requestValues);
   }
