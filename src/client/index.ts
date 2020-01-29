b69aa7029b028883be198bf68ba3cf4a1e00158e
CapitanFindusFI <am95.dev@gmail.com>
2020-01-29 15:09:49 +0100

    updated generator stuff and tests

--- src/client/index.ts
+++ src/client/index.ts
@@ -1,7 +1,7 @@
-import axios, { AxiosInstance, AxiosRequestConfig } from 'axios';
-import { GraphQLOperationType } from '../enums';
-import { GraphQLGenerator } from './graphql-generator';
-import { GraphQLRequest } from './graphql-request';
+import axios, { AxiosInstance, AxiosRequestConfig } from "axios";
+import { GraphQLOperationType } from "../enums";
+import { GraphQLGenerator } from "./graphql-generator";
+import { GraphQLRequest } from "./graphql-request";
 
 export class GraphAPIClient {
   private axios: AxiosInstance;
@@ -13,36 +13,36 @@ export class GraphAPIClient {
   public get<T>(path: string, params = {}, headers = {}): Promise<T> {
     return this.axios.get(path, {
       ...params,
-      ...headers,
+      ...headers
     });
   }
 
   public post<T>(path: string, body: any, params = {}, headers = {}): Promise<T> {
     return this.axios.post(path, body, {
       ...params,
-      ...headers,
+      ...headers
     });
   }
 
   public collectRequestBody(
     operationType: GraphQLOperationType,
     ...requests: GraphQLRequest[]
-  ): { query: string; variables: Map<string, any> } {
+  ): { query: string; variables: { [p: string]: any } } {
     const generator = new GraphQLGenerator(operationType, ...requests);
     const query = generator.generateQueryString();
     const variables = generator.values;
     return {
       query,
-      variables,
+      variables
     };
   }
 
-  public query<T>(path: string = '/graphql', ...requests: GraphQLRequest[]): Promise<T> {
+  public query<T>(path: string = "/graphql", ...requests: GraphQLRequest[]): Promise<T> {
     const body = this.collectRequestBody(GraphQLOperationType.QUERY, ...requests);
     return this.post(path, body);
   }
 
-  public mutate<T>(path: string = '/graphql', ...requests: GraphQLRequest[]): Promise<T> {
+  public mutate<T>(path: string = "/graphql", ...requests: GraphQLRequest[]): Promise<T> {
     const body = this.collectRequestBody(GraphQLOperationType.MUTATION, ...requests);
     return this.post(path, body);
   }
