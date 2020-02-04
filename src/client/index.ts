896b120e88e6d5d44d94c53d384c5c0a6616ebe2
CapitanFindusFI <am95.dev@gmail.com>
2020-02-04 11:59:21 +0100

    updated client allowing passing an AxiosRequestConfig as second parameter, allowing editing of request config for a single request

--- src/client/index.ts
+++ src/client/index.ts
@@ -1,12 +1,12 @@
-import axios, { AxiosInstance, AxiosRequestConfig } from 'axios';
-import { GraphQLOperationType } from '../enums';
-import { IGraphClientConfig } from '../interfaces';
-import { GraphQLGenerator } from './graphql-generator';
-import { GraphQLRequest } from './graphql-request';
+import axios, { AxiosInstance, AxiosRequestConfig } from "axios";
+import { GraphQLOperationType } from "../enums";
+import { IGraphClientConfig } from "../interfaces";
+import { GraphQLGenerator } from "./graphql-generator";
+import { GraphQLRequest } from "./graphql-request";
 
 const defaultClientConfig: IGraphClientConfig = {
-  queryPayloadName: 'query',
-  variablesPayloadName: 'variables',
+  queryPayloadName: "query",
+  variablesPayloadName: "variables"
 };
 
 export class GraphAPIClient {
@@ -18,18 +18,12 @@ export class GraphAPIClient {
     this.config = Object.assign({}, defaultClientConfig, clientConfig);
   }
 
-  public get<T>(path: string, params = {}, headers = {}): Promise<T> {
-    return this.axios.get(path, {
-      ...params,
-      ...headers,
-    });
+  public get<T>(path: string, config: AxiosRequestConfig = {}): Promise<T> {
+    return this.axios.get(path, { ...config });
   }
 
-  public post<T>(path: string, body: any, params = {}, headers = {}): Promise<T> {
-    return this.axios.post(path, body, {
-      ...params,
-      ...headers,
-    });
+  public post<T>(path: string, body: any, config: AxiosRequestConfig = {}): Promise<T> {
+    return this.axios.post(path, body, { ...config });
   }
 
   public collectRequestBody(operationType: GraphQLOperationType, ...requests: GraphQLRequest[]): { [p: string]: any } {
@@ -40,17 +34,17 @@ export class GraphAPIClient {
 
     return {
       [queryKeyName]: generator.generateQueryString(),
-      [variablesKeyName]: generator.generateQueryValues(),
+      [variablesKeyName]: generator.generateQueryValues()
     };
   }
 
-  public query<T>(path: string = '/graphql', ...requests: GraphQLRequest[]): Promise<T> {
+  public query<T>(path: string = "/graphql", config: AxiosRequestConfig = {}, ...requests: GraphQLRequest[]): Promise<T> {
     const body = this.collectRequestBody(GraphQLOperationType.QUERY, ...requests);
-    return this.post(path, body);
+    return this.post(path, body, config);
   }
 
-  public mutate<T>(path: string = '/graphql', ...requests: GraphQLRequest[]): Promise<T> {
+  public mutate<T>(path: string = "/graphql", config: AxiosRequestConfig = {}, ...requests: GraphQLRequest[]): Promise<T> {
     const body = this.collectRequestBody(GraphQLOperationType.MUTATION, ...requests);
-    return this.post(path, body);
+    return this.post(path, body, config);
   }
 }
