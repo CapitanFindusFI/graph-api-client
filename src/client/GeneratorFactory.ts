251c13860f357012f32f65f3aa9f6048734e1964
CapitanFindusFI <am95.dev@gmail.com>
2020-04-07 17:53:15 +0200

    1.0.13

--- src/client/GeneratorFactory.ts
+++ src/client/GeneratorFactory.ts
@@ -1,18 +1,18 @@
-import GraphqlMutationGenerator from "graphql-request-generator/lib/src/generator/graphql-mutation.generator";
-import GraphqlQueryGenerator from "graphql-request-generator/lib/src/generator/graphql-query.generator";
-import {GraphQLRequestType} from "../types/GraphQLRequestType";
+import GraphqlMutationGenerator from 'graphql-request-generator/lib/src/generator/graphql-mutation.generator';
+import GraphqlQueryGenerator from 'graphql-request-generator/lib/src/generator/graphql-query.generator';
+import { GraphQLRequestType } from '../types/GraphQLRequestType';
 
 class GeneratorFactory {
-    public static getGenerator(requestType: GraphQLRequestType) {
-        switch (requestType) {
-            case "mutation":
-                return new GraphqlMutationGenerator();
-            case "query":
-                return new GraphqlQueryGenerator();
-            default:
-                throw new Error(`Unknown request type: ${requestType}`);
-        }
+  public static getGenerator(requestType: GraphQLRequestType) {
+    switch (requestType) {
+      case 'mutation':
+        return new GraphqlMutationGenerator();
+      case 'query':
+        return new GraphqlQueryGenerator();
+      default:
+        throw new Error(`Unknown request type: ${requestType}`);
     }
+  }
 }
 
 export default GeneratorFactory;
